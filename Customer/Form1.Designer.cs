47fa4845b44d711030bf691aed69c4c9b4e2469b
YusufAtti <yusuyatti@gmail.com>
2024-02-04 17:07:04 +0300

    Proje dosyası ekle.

---
+++ Customer/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace Customer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.customerAutomationDataSet = new Customer.CustomerAutomationDataSet();
+            this.customerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.customerTableAdapter = new Customer.CustomerAutomationDataSetTableAdapters.CustomerTableAdapter();
+            this.musteriIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.adDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.soyadDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.aylikGelirDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.krediyeUygunMuDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.sehirDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtsurname = new System.Windows.Forms.TextBox();
+            this.txtcity = new System.Windows.Forms.TextBox();
+            this.txtcredit = new System.Windows.Forms.TextBox();
+            this.txtsalary = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerAutomationDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Location = new System.Drawing.Point(29, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(743, 285);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BackColor = System.Drawing.Color.Peru;
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.txtcity);
+            this.panel2.Controls.Add(this.txtcredit);
+            this.panel2.Controls.Add(this.txtsalary);
+            this.panel2.Controls.Add(this.txtsurname);
+            this.panel2.Controls.Add(this.txtname);
+            this.panel2.Controls.Add(this.txtid);
+            this.panel2.Location = new System.Drawing.Point(29, 317);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(743, 158);
+            this.panel2.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.musteriIDDataGridViewTextBoxColumn,
+            this.adDataGridViewTextBoxColumn,
+            this.soyadDataGridViewTextBoxColumn,
+            this.aylikGelirDataGridViewTextBoxColumn,
+            this.krediyeUygunMuDataGridViewCheckBoxColumn,
+            this.sehirDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.customerBindingSource;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(743, 285);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // customerAutomationDataSet
+            // 
+            this.customerAutomationDataSet.DataSetName = "CustomerAutomationDataSet";
+            this.customerAutomationDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // customerBindingSource
+            // 
+            this.customerBindingSource.DataMember = "Customer";
+            this.customerBindingSource.DataSource = this.customerAutomationDataSet;
+            // 
+            // customerTableAdapter
+            // 
+            this.customerTableAdapter.ClearBeforeFill = true;
+            // 
+            // musteriIDDataGridViewTextBoxColumn
+            // 
+            this.musteriIDDataGridViewTextBoxColumn.DataPropertyName = "MusteriID";
+            this.musteriIDDataGridViewTextBoxColumn.HeaderText = "MusteriID";
+            this.musteriIDDataGridViewTextBoxColumn.Name = "musteriIDDataGridViewTextBoxColumn";
+            this.musteriIDDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // adDataGridViewTextBoxColumn
+            // 
+            this.adDataGridViewTextBoxColumn.DataPropertyName = "Ad";
+            this.adDataGridViewTextBoxColumn.HeaderText = "Ad";
+            this.adDataGridViewTextBoxColumn.Name = "adDataGridViewTextBoxColumn";
+            // 
+            // soyadDataGridViewTextBoxColumn
+            // 
+            this.soyadDataGridViewTextBoxColumn.DataPropertyName = "Soyad";
+            this.soyadDataGridViewTextBoxColumn.HeaderText = "Soyad";
+            this.soyadDataGridViewTextBoxColumn.Name = "soyadDataGridViewTextBoxColumn";
+            // 
+            // aylikGelirDataGridViewTextBoxColumn
+            // 
+            this.aylikGelirDataGridViewTextBoxColumn.DataPropertyName = "AylikGelir";
+            this.aylikGelirDataGridViewTextBoxColumn.HeaderText = "AylikGelir";
+            this.aylikGelirDataGridViewTextBoxColumn.Name = "aylikGelirDataGridViewTextBoxColumn";
+            // 
+            // krediyeUygunMuDataGridViewCheckBoxColumn
+            // 
+            this.krediyeUygunMuDataGridViewCheckBoxColumn.DataPropertyName = "KrediyeUygunMu";
+            this.krediyeUygunMuDataGridViewCheckBoxColumn.HeaderText = "KrediyeUygunMu";
+            this.krediyeUygunMuDataGridViewCheckBoxColumn.Name = "krediyeUygunMuDataGridViewCheckBoxColumn";
+            // 
+            // sehirDataGridViewTextBoxColumn
+            // 
+            this.sehirDataGridViewTextBoxColumn.DataPropertyName = "Sehir";
+            this.sehirDataGridViewTextBoxColumn.HeaderText = "Sehir";
+            this.sehirDataGridViewTextBoxColumn.Name = "sehirDataGridViewTextBoxColumn";
+            // 
+            // txtid
+            // 
+            this.txtid.Enabled = false;
+            this.txtid.Location = new System.Drawing.Point(128, 13);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(122, 22);
+            this.txtid.TabIndex = 0;
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(128, 59);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(122, 22);
+            this.txtname.TabIndex = 1;
+            // 
+            // txtsurname
+            // 
+            this.txtsurname.Location = new System.Drawing.Point(128, 108);
+            this.txtsurname.Name = "txtsurname";
+            this.txtsurname.Size = new System.Drawing.Size(122, 22);
+            this.txtsurname.TabIndex = 2;
+            // 
+            // txtcity
+            // 
+            this.txtcity.Location = new System.Drawing.Point(386, 108);
+            this.txtcity.Name = "txtcity";
+            this.txtcity.Size = new System.Drawing.Size(122, 22);
+            this.txtcity.TabIndex = 5;
+            // 
+            // txtcredit
+            // 
+            this.txtcredit.Location = new System.Drawing.Point(386, 59);
+            this.txtcredit.Name = "txtcredit";
+            this.txtcredit.Size = new System.Drawing.Size(122, 22);
+            this.txtcredit.TabIndex = 4;
+            // 
+            // txtsalary
+            // 
+            this.txtsalary.Location = new System.Drawing.Point(386, 13);
+            this.txtsalary.Name = "txtsalary";
+            this.txtsalary.Size = new System.Drawing.Size(122, 22);
+            this.txtsalary.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Customer ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Name: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Surname: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(324, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Salary: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(327, 62);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Credit: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(341, 108);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "City: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 487);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerAutomationDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel2;
+        private CustomerAutomationDataSet customerAutomationDataSet;
+        private System.Windows.Forms.BindingSource customerBindingSource;
+        private CustomerAutomationDataSetTableAdapters.CustomerTableAdapter customerTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn musteriIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn adDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn soyadDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn aylikGelirDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn krediyeUygunMuDataGridViewCheckBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sehirDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtcity;
+        private System.Windows.Forms.TextBox txtcredit;
+        private System.Windows.Forms.TextBox txtsalary;
+        private System.Windows.Forms.TextBox txtsurname;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Customer/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Customer
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connect = new SqlConnection(@"Data Source=DESKTOP-HP2NA5H\SQLEXPRESS;Initial Catalog=CustomerAutomation;Integrated Security=True");
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.customerTableAdapter.Fill(this.customerAutomationDataSet.Customer);
+            ShowData();
+            dataGridView1.ClearSelection();
+            txtid.Text = "0";
+        }
+
+        private void ShowData()
+        {
+
+            try {
+                string show = "SELECT MusteriID, Ad, Soyad, AylikGelir, KrediyeUygunMu, Sehir FROM Customer";
+                SqlDataAdapter dataadapter = new SqlDataAdapter(show, connect);
+                DataTable dt = new DataTable();
+                dataadapter.Fill(dt);
+
+
+                if(dt.Rows.Count > 0)
+                {
+                    dataGridView1.DataSource = dt;
+                }
+
+            }
+
+            catch(Exception ex)
+            {
+                MessageBox.Show("Hata!! KOD: 4001");
+            }
+
+            finally
+            {
+                if (connect != null)
+                    connect.Close();
+            }
+
+         }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int secilensatir = dataGridView1.SelectedCells[0].RowIndex;
+            txtid.Text = dataGridView1.Rows[secilensatir].Cells[0].Value.ToString();
+            txtname.Text = dataGridView1.Rows[secilensatir].Cells[1].Value.ToString();
+            txtsurname.Text = dataGridView1.Rows[secilensatir].Cells[2].Value.ToString();
+            txtsalary.Text = dataGridView1.Rows[secilensatir].Cells[3].Value.ToString();
+            txtcredit.Text = dataGridView1.Rows[secilensatir].Cells[4].Value.ToString();
+            txtcity.Text = dataGridView1.Rows[secilensatir].Cells[5].Value.ToString();
+        }
+    }
+}
